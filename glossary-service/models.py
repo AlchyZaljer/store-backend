d5a82e97c1e1701382d3bae8e3c8e5652ca99c00
Alchy Zaljer <alchy_zaljer@mail.ru>
2024-12-13 16:08:45 +0300

    Update product model

--- glossary-service/models.py
+++ glossary-service/models.py
@@ -6,6 +6,6 @@ class Product(Base):
     __tablename__ = "products"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(255), index=True)
-    price = Column(Float)
+    name = Column(String(255), nullable=False, index=True)
+    price = Column(Float, nullable=False)
     description = Column(String(500), nullable=True)
