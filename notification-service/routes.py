83edf5d201ba3d64abf744ca0e599739c71a6e02
Alchy Zaljer <natalie.ambartsumova@gmail.com>
2024-12-15 01:27:35 +0300

    notification-service: Update routes and their tests

--- notification-service/routes.py
+++ notification-service/routes.py
@@ -14,18 +14,19 @@ def root():
     return {"message": "Notification Service API"}
 
 
-@router.get("/notifications/last", response_model=NotificationResponse)
-async def get_last_notification(db=Depends(get_db)):
-    notification = await db.notifications.find_one(sort=[("timestamp", -1)])
-    if not notification:
-        raise HTTPException(status_code=404, detail="No notifications found")
-    notification["id"] = str(notification["_id"])
-    return notification
-
-
 @router.get("/notifications/recent", response_model=list[NotificationResponse])
-async def get_recent_notifications(db=Depends(get_db)):
-    notifications = await db.notifications.find(sort=[("timestamp", -1)]).to_list(5)
+async def get_recent_notifications(
+        count: int = Query(
+            default=1,
+            gt=0,
+            description="Number of most recent notifications (must be greater than 0)"
+        ),
+        db=Depends(get_db)
+):
+    notifications = await db.notifications.find(sort=[("timestamp", -1)]).limit(count).to_list(None)
+    if not notifications:
+        raise HTTPException(status_code=404, detail="No notifications found")
+
     for notification in notifications:
         notification["id"] = str(notification["_id"])
     return notifications
@@ -46,7 +47,7 @@ async def get_notifications_after(
     except ValueError:
         raise HTTPException(status_code=400, detail="Invalid date format. Use 'YYYY-MM-DD' or 'YYYY-MM-DD HH:MM:SS'")
 
-    notifications = await db.notifications.find({"timestamp": {"$gt": parsed_time}}).to_list(100)
+    notifications = await db.notifications.find({"timestamp": {"$gt": parsed_time}}).to_list(None)
 
     for notification in notifications:
         notification["id"] = str(notification["_id"])
