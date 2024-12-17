727a38afd97282741934f90138e3d66b5f4c1ed3
Alchy Zaljer <natalie.ambartsumova@gmail.com>
2024-12-18 00:27:55 +0300

    order-service: Fix publisher's connection to rabbitmq

--- order-service/publisher.py
+++ order-service/publisher.py
@@ -6,25 +6,34 @@ import pika
 RABBITMQ_HOST = "localhost"
 RABBITMQ_QUEUE = "notifications"
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
-channel = connection.channel()
-channel.queue_declare(queue=RABBITMQ_QUEUE, durable=True)
+connection = None
+channel = None
+
+
+def get_connection():
+    global connection, channel
+    if not connection or connection.is_closed:
+        connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
+        channel = connection.channel()
+        channel.queue_declare(queue=RABBITMQ_QUEUE, durable=True)
+    return channel
 
 
 def publish_to_rabbitmq(message: dict):
     try:
+        channel = get_connection()
         channel.basic_publish(
             exchange='',
             routing_key=RABBITMQ_QUEUE,
             body=json.dumps(message),
             properties=pika.BasicProperties(
-                delivery_mode=1,
+                delivery_mode=2,
             )
         )
 
         print(f"Publish order {message['id']} notification")
     except Exception as e:
         print(f"Error while publishing message to RabbitMQ: {e}")
-    finally:
-        if connection:
+
+        if connection and connection.is_open:
             connection.close()
